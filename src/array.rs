15587c4c16c94f0687810156427f384b7b853c6a
phil <uniphil@gmail.com>
2025-05-09 19:06:32 -0400

    vector-friendly array search for insert

--- src/array.rs
+++ src/array.rs
@@ -15,23 +15,50 @@ pub(crate) const MAX_CAPACITY: usize = 128;
 
 /// Array representation container
 #[cfg_attr(feature = "with_serde", derive(Serialize, Deserialize))]
-pub(crate) struct Array<const P: usize, const W: usize>(Vec<u32>);
+pub(crate) struct Array<const P: usize, const W: usize>(Vec<u32>, usize);
 
 impl<const P: usize, const W: usize> Array<P, W> {
     /// Insert encoded hash into `Array` representation
     /// Returns true on success, false otherwise.
     #[inline]
     pub(crate) fn insert(&mut self, h: u32) -> bool {
-        if {
-            let mut res = false;
-            for x in &self.0 {
-                res |= *x == h
+        // 1. search
+        let found = match self.0.len() {
+            4 => contains_fixed_hopefully_vectorized::<4>(
+                self.0.as_slice().try_into().expect("vec of len 4 can become array of len 4"),
+                h,
+            ),
+            8 => contains_fixed_hopefully_vectorized::<8>(
+                self.0.as_slice().try_into().expect("vec of len 4 can become array of len 4"),
+                h,
+            ),
+            n => {
+                assert_eq!(n % 16, 0);
+                self.0
+                    .chunks_exact(16)
+                    .any(|chunk| contains_fixed_hopefully_vectorized::<16>(
+                        chunk.try_into().unwrap(),
+                        h,
+                    ))
             }
-            res
-        } {
+        };
+
+        if found {
+            return true;
+        }
+
+        // 2. insert new item
+        let l = self.0.len();
+        if self.1 > 0 {
+            self.0[l-self.1] = h;
+            self.1 -= 1;
             true
-        } else if self.0.len() < MAX_CAPACITY {
-            self.0.push(h);
+        } else if l < MAX_CAPACITY {
+            // assert_eq!(l % 4, 0);
+            self.0.reserve_exact(l * 2);
+            self.0.resize(l * 2, 0);
+            self.0[l] = h;
+            self.1 = l - 1;
             true
         } else {
             false
@@ -41,7 +68,7 @@ impl<const P: usize, const W: usize> Array<P, W> {
     /// Create new instance of `Array` representation from vector
     #[inline]
     pub(crate) fn from_small(a: u32, b: u32, c: u32) -> Array<P, W> {
-        Self(vec![a, b, c])
+        Self(vec![a, b, c, 0], 1)
     }
 }
 
@@ -62,7 +89,7 @@ impl<const P: usize, const W: usize> RepresentationTrait<P, W> for Array<P, W> {
     /// Return cardinality estimate of `Array` representation
     #[inline]
     fn estimate(&self) -> usize {
-        self.0.len()
+        self.0.len() - self.1
     }
 
     /// Return memory size of `Array` representation
@@ -88,8 +115,18 @@ impl<const P: usize, const W: usize> Deref for Array<P, W> {
     type Target = [u32];
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.0.get(0..self.0.len() - self.1).expect("alkjdfl")
     }
+}
+
+/// Vectorized linear fixed array search
+#[inline]
+fn contains_fixed_hopefully_vectorized<const N: usize>(a: [u32; N], v: u32) -> bool {
+    let mut res = false;
+    for x in a {
+        res |= x == v
+    }
+    res
 }
 
 #[cfg(test)]
@@ -98,6 +135,6 @@ mod tests {
 
     #[test]
     fn array_size() {
-        assert_eq!(std::mem::size_of::<Array<0, 0>>(), 24);
+        assert_eq!(std::mem::size_of::<Array<0, 0>>(), 32);
     }
 }
